3b26b3b21d2bc6f6deb61b4a83ee5ef10e444187
Rahul Vishvakarma <vskrahul@Rahuls-Laptop.lan>
2024-10-23 19:09:45 -0500

    code cleanup

--- first-angular-app/src/app/standalone/user/standalone.user.component.ts
+++ first-angular-app/src/app/standalone/user/standalone.user.component.ts
@@ -1,13 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, Signal, signal, WritableSignal } from "@angular/core";
 import { DUMMY_USERS } from "../../dummy-users";
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { CommonModule } from "@angular/common";
 
 @Component({
     selector: 'standalone-app-user',
     standalone: true,
+    imports: [
+      CommonModule
+    ],
     templateUrl: './standalone.user.component.html'
   })
 export class StandaloneUserComponent{
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[0]);
+  imagePath = '../assets/user/' + this.selectedUser().avatar;
+
+  selectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    // this.selectedUser = DUMMY_USERS[randomIndex];
+  }
 }
